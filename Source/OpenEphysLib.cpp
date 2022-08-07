ad5fd4d2b0f67c824cfb811277a4239990a1c7de
Josh Siegle <joshs@alleninstitute.org>
2022-08-07 15:53:27 -0700

    Update `libVersion` to 0.2.1

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -43,7 +43,7 @@ extern "C" EXPORT void getLibInfo(Plugin::LibraryInfo* info)
 {
 	info->apiVersion = PLUGIN_API_VER;
 	info->name = "Rhythm Plugins";
-	info->libVersion = "0.2.0";
+	info->libVersion = "0.2.1";
 	info->numPlugins = NUM_PLUGINS;
 }
 
