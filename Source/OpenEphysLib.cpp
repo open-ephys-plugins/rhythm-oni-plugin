d5c1c2357b5083886ad3d607de1562418c53a291
anjaldoshi <anjaldoshi09@gmail.com>
2023-04-11 17:27:28 -0700

    Bump plugin version to 0.2.0

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -41,7 +41,7 @@ extern "C" EXPORT void getLibInfo(Plugin::LibraryInfo* info)
 {
 	info->apiVersion = PLUGIN_API_VER;
 	info->name = "OE FPGA Acquisition Board";
-	info->libVersion = "0.1.6";
+	info->libVersion = "0.2.0";
 	info->numPlugins = NUM_PLUGINS;
 }
 
